6fc5a1d2717f5b57ee394a5bf6bd00bbeb2adb2f
neznaika0 <ozornick.ks@gmail.com>
2025-02-03 09:51:02 +0300

    fix: Add finder for php-cs-fixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -4,9 +4,18 @@ declare(strict_types=1);
 
 use CodeIgniter\CodingStandard\CodeIgniter4;
 use Nexus\CsConfig\Factory;
+use PhpCsFixer\Finder;
+
+$finder = Finder::create()
+    ->files()
+    ->in([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ]);
 
 $options = [
     'cacheFile' => './build/.php-cs-fixer.cache',
+    'finder'    => $finder,
 ];
 
 return Factory::create(new CodeIgniter4(), [], $options)->forProjects();
